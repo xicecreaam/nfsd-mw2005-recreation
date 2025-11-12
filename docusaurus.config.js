32e45f7e63c28c649f8d24a9e2002e35caac9b5a
cataresk 😇 <codqg@icecream.home>
2025-11-12 01:34:50 +0100

    Updated site content

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,9 +10,9 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: 'Welcome to Need for SpeedDocs Reborn',
+  tagline: 'This website is aiming to rehost speedrunning documentation for most Need for Speed games that has been shut down by Balathruin himself, creator of the original website. Currently looking for contributors to expand.',
+  favicon: 'img/nfsd-logo.png',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -20,17 +20,18 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://xicecreaam.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/nfsd-recreation/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'xicecreaam', // Usually your GitHub org/user name.
+  projectName: 'nfsd-recreation', // Usually your repo name.
 
   onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
@@ -78,27 +79,30 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/nfsd-logo.png',
       colorMode: {
         respectPrefersColorScheme: true,
       },
       navbar: {
-        title: 'My Site',
+        title: 'NFSD',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'NFSD logo',
+          src: 'img/nfsd-logo.png',
         },
         items: [
           {
             type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            sidebarId: 'gamesSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Underground 1)',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          {to: '/UG2', label: 'Underground 2', position: 'left'},
+          {to: '/MW05', label: 'Most Wanted (2005)', position: 'left'},
+          {to: '/C', label: 'Carbon', position: 'left'},
+          {to: '/PS', label: 'ProStreet', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
+            href: 'https://linktr.ee/xicecreaam',
+            label: 'Linktree',
             position: 'right',
           },
         ],
