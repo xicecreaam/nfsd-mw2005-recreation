fc2d0cfb4c7805715bd37288dd55ebe21f7afb92
icecream <codqg99@gmail.com>
2026-09-06 18:57:50 +0200

    Website logo update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'Need for SpeedDocs Reborn',
   tagline: 'This website is aiming to rehost speedrunning documentation for most Need for Speed games that has been shut down by Balathruin himself, creator of the original website. Currently looking for contributors to expand.',
-  favicon: 'img/nfsd-logo-v2.png',
+  favicon: 'img/nfsdr-logo-v2.png',
 
   future: {
     v4: true,
@@ -43,7 +43,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      image: 'img/nfsd-logo.gif',
+      image: 'img/nfsdr-logo-v2.png',
       colorMode: {
         defaultMode: 'dark',
         disableSwitch: true,
@@ -51,8 +51,8 @@ const config = {
       },
       navbar: {
         logo: {
-          alt: 'NFSD logo',
-          src: 'img/nfsdr-logo.gif',},
+          alt: 'NFSDR logo',
+          src: 'img/nfsdr-logo-v2.png',},
           
         items: [
           {to: 'ug1', label: 'Underground 1', position: 'left' },
